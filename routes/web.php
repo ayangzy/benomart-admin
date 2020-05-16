3c3f45e2948e456055dca054605ed199900c1004
ayangzy <ayangefelix8@gmail.com>
2020-05-16 09:33:56 -0700

    created categories model, controller and all controllers routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,76 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('dashboard');
+
+Route::group(['prefix' => 'admins', 'middleware' => 'auth'], function(){
+
+    Route::resource('categories', 'CategoriesController');
+
+    Route::resource('subcategories', 'SubcategoriesController');
+
+    Route::resource('products', 'ProductController');
+
+    Route::resource('profile', 'ProfileController');
+
+    Route::get('products/firstimage/{slug}', [
+        'uses' => 'ProductController@firstImage',
+        'as' => 'products.firstimage'
+    ]);
+
+
+    Route::post('products/updateFirstImage/{slug}', [
+        'uses' => 'ProductController@updateFirstImage',
+        'as' => 'products.updateFirstImage'
+    ]);
+
+
+
+    Route::get('products/secondimage/{slug}', [
+        'uses' => 'ProductController@secondImage',
+        'as' => 'products.secondimage'
+    ]);
+
+    Route::post('products/updateSecondImage/{slug}', [
+        'uses' => 'ProductController@updateSecondImage',
+        'as' => 'products.updateSecondImage'
+    ]);
+
+
+    Route::get('products/thirdimage/{slug}', [
+        'uses' => 'ProductController@thirdImage',
+        'as' => 'products.thirdimage'
+    ]);
+
+    Route::post('products/updateThirdImage/{slug}', [
+        'uses' => 'ProductController@updateThirdImage',
+        'as' => 'products.updateThirdImage'
+    ]);
+
+    Route::get('/change-password', [
+        'uses'=> 'AdminsController@password',
+        'as' => 'change-password',
+    ]);
+
+    Route::post('/change-password', [
+        'uses'=> 'AdminsController@newPassword',
+        'as' => 'newPassword',
+    ]);
+
+
+    // Route::post('/ajax-subcat', function(Request $request){
+
+    //     $cat_id = $request->cat_id;
+    //     $subcategories = Category::where('category_id', '=', $cat_id)->get();
+    //     return response()->json($categories, 200);
+
+    // });
+
+
+
+    // Route::get('subcategories/create', [
+    //     'uses'=> 'SubcategoriesController@create',
+    //     'as' => 'subcategories.create',
+    // ]);
+
+
+    });
